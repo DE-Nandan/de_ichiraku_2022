51653b105b3e210e5f3b1e26c7018bab8465606b
Nandan Kumar <nandankmrde@gmail.com>
2022-11-08 20:10:37 +0530

    ratingDn

--- app/src/main/java/com/example/de_ichiraku/UserActivity/FeedbackActivity.java
+++ app/src/main/java/com/example/de_ichiraku/UserActivity/FeedbackActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.LayerDrawable;
@@ -11,19 +12,26 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RatingBar;
+import android.widget.Toast;
 
+import com.example.de_ichiraku.Prevalent.Prevalent;
 import com.example.de_ichiraku.R;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
 
 public class FeedbackActivity extends AppCompatActivity {
 
     Button fbutton;
     String Rating;
     RatingBar rt;
+    String prodID;
+    int size;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,10 +43,38 @@ public class FeedbackActivity extends AppCompatActivity {
         LayerDrawable stars=(LayerDrawable)rt.getProgressDrawable();
 
         Rating = String.valueOf(rt.getRating());
+        Intent intent = getIntent();
+        prodID  =intent.getStringExtra("prodID");
         //Use for changing the color of RatingBar
         stars.getDrawable(2).setColorFilter(Color.RED, PorterDuff.Mode.SRC_ATOP);
 
-        DatabaseReference prodRef = FirebaseDatabase.getInstance().getReference().child("Products");
+        DatabaseReference prodRef = FirebaseDatabase.getInstance().getReference().child("Products").child(prodID);
+        DatabaseReference ratProdRef = prodRef.child("userR").child(Prevalent.currentOnlineUsers.getPhone());
+        DatabaseReference userRef = prodRef.child("userR");
+
+
+       HashMap <String,Object> rat = new HashMap<>();
+       rat.put("rating","0");
+       rat.put("state","no");
+
+       ratProdRef.updateChildren(rat);
+
+       userRef.addListenerForSingleValueEvent(new ValueEventListener() {
+           @Override
+           public void onDataChange(@NonNull DataSnapshot snapshot) {
+               size = (int) snapshot.getChildrenCount();
+           }
+
+           @Override
+           public void onCancelled(@NonNull DatabaseError error) {
+
+           }
+       });
+
+
+
+
+
 
 
 
@@ -52,24 +88,33 @@ public class FeedbackActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                prodRef.addChildEventListener(new ChildEventListener() {
+                ratProdRef.addValueEventListener(new ValueEventListener() {
                     @Override
-                    public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-                        snapshot.getRef().child("rating").setValue(rt.getRating()/5);
-                    }
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                      // String ratingState = snapshot.child("state").getValue().toString();
+                       // String userName = snapshot.child("name").getValue().toString();
+                        //int usrR = (int) snapshot.child("rating").getValue();
 
-                    @Override
-                    public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                        double usrR = Double.valueOf(String.valueOf(snapshot.child("rating").getValue()));
+                       // Toast.makeText(FeedbackActivity.this, usrR, Toast.LENGTH_SHORT).show();
+                              prodRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                                  @Override
+                                  public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                      Double prevR = 0.0;
+                                      if(snapshot.child("rating").exists())
+                                      prevR = Double.valueOf(String.valueOf(snapshot.child("rating").getValue()));
 
-                    }
+                                      snapshot.getRef().child("rating").setValue((prevR+rt.getRating()-usrR)/size);
+                                     // snapshot.getRef().child("state").setValue("yes");
+                                  }
 
-                    @Override
-                    public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+                                  @Override
+                                  public void onCancelled(@NonNull DatabaseError error) {
 
-                    }
+                                  }
+                              });
+                             snapshot.getRef().child("rating").setValue(rt.getRating());
 
-                    @Override
-                    public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
 
                     }
 
--- app/src/main/java/com/example/de_ichiraku/UserActivity/ProductDetailsActivity.java
+++ app/src/main/java/com/example/de_ichiraku/UserActivity/ProductDetailsActivity.java
@@ -80,6 +80,7 @@ public class ProductDetailsActivity extends AppCompatActivity {
              @Override
              public void onClick(View v) {
                Intent intent = new Intent(ProductDetailsActivity.this,FeedbackActivity.class);
+               intent.putExtra("prodID",productID);
                startActivity(intent);
              }
          });
